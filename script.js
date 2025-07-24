fb6189949832a8813e27f81da21466b849f83679
Yusra Javed <yusrajaved56@gmail.com>
2025-07-24 15:20:20 +0500

    edited

---
+++ script.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FF8080',
+                secondary: '#98FF98',
+                softYellow: '#FFE5B4',
+            },
+            borderRadius: {
+                'none': '0px',
+                'sm': '4px',
+                DEFAULT: '8px',
+                'md': '12px',
+                'lg': '16px',
+                'xl': '20px',
+                '2xl': '24px',
+                '3xl': '32px',
+                'full': '9999px',
+                'button': '8px'
+            }
+        }
+    }
+}
